6ee4a7ca35e280636c6b07d995b798da8dd5aa3c
varun <varunlad5@gmail.com>
2021-09-02 23:36:43 +0530

    [varun] OOPs concepts

---
+++ OOPS/Ecapsulation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS
+{
+    public class Bank
+    {
+        //hiding class data by declaring the variable as private
+        private double balance;
+        //creating public setter and getter methods
+        public double getBalance()
+        {
+            //add validation logic if needed
+            return balance;
+        }
+        public void setBalance(double balance)
+        {
+            // add validation logic to check whether data is correct or not
+            this.balance = balance;
+        }
+    }
+    class Ecapsulation
+    {
+        public static void Main()
+        {
+            Bank Boi = new Bank();
+            Boi.setBalance(500);
+            Console.WriteLine(Boi.getBalance());
+            
+            Console.ReadLine();
+        }
+    }
+}
--- OOPS/abstraction.cs
+++ OOPS/abstraction.cs
@@ -7,35 +7,42 @@ using System.Threading.Tasks;
 namespace OOPS
 {
     // Abstract class
-    abstract class Animal
-    {
-        // Abstract method (does not have a body)
-        public abstract void animalSound();
-        // Regular method
-        public void sleep()
-        {
-            Console.WriteLine("Zzz");
-        }
-    }
+abstract class Animal
+{
+  // Abstract method (does not have a body)
+  public abstract void animalSound();
+  // Regular method
+  public void sleep()
+  {
+    Console.WriteLine("Zzz");
+  }
+}
 
-    // Derived class (inherit from Animal)
-    class Pig : Animal
-    {
-        public override void animalSound()
-        {
-            // The body of animalSound() is provided here
-            Console.WriteLine("The pig says: wee wee");
-        }
-    }
+// Derived class (inherit from Animal)
+class Pig : Animal
+{
+  public override void animalSound()
+  {
+    // The body of animalSound() is provided here
+    Console.WriteLine("The pig says: wee wee");
+  }
+}
 
-    class animal
-    {
-        static void Main(string[] args)
-        {
-            Pig myPig = new Pig();  // Create a Pig object
-            myPig.animalSound();
-            myPig.sleep();
-            Console.ReadLine();
-        }
-    }
+class Abstraction
+{
+  static void Main(string[] args)
+  {
+    Pig myPig = new Pig(); // Create a Pig object
+    myPig.animalSound();  // Call the abstract method
+    myPig.sleep();  // Call the regular method
+    Console.ReadLine();
+  }
+} 
+ 
+ 
+ 
+ 
+ 
+
+
 }
